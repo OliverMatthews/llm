73fba10a1ce93a375523cef477e4514e168d9ff3
Oliver Pelling-Matthews <oliver@pellingmatthews.co.uk>
2024-11-19 01:26:31 +0000

    Response streaming

--- ollama/llama3.2-11b.py
+++ ollama/llama3.2-11b.py
@@ -4,8 +4,10 @@ response = ollama.chat(
     model='llama3.2-vision',
     messages=[{
         'role': 'user',
-        'content': 'Hi, what can you do?'
-    }]
+        'content': 'Give me a concise explainer on how humans can establish a permanent presence on the moon'
+    }],
+    stream=True
 )
 
-print(response)
+for chunk in response:
+    print(chunk['message']['content'], end='', flush=True)
